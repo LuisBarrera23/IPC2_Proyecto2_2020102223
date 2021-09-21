73941945611d8b8098fce0b9e64a5bb419d654e3
LuisBarrera23 <78067073+LuisBarrera23@users.noreply.github.com>
2021-09-21 01:09:08 -0600

    interface basica

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from tkinter import Tk
+from tkinter import Button, Tk,ttk
 
 
 #variables globales
@@ -18,6 +18,26 @@ def generarVentana():
     y=(y-alto)/2
     ventana.geometry('%dx%d+%d+%d' % (ancho, alto, x, y))
     ventana.title("Ensambladora")
+
+    config = ttk.Style()
+    config.configure('TFrame', background='#008080')
+    pestañas=ttk.Notebook(ventana)
+    pestaña1 = ttk.Frame(pestañas,style="TFrame")
+    pestaña2 = ttk.Frame(pestañas)
+    pestaña3 = ttk.Frame(pestañas)
+
+    
+    pestañas.add(pestaña1, text="Cargar Archivo")
+    pestañas.add(pestaña2, text="Reportes")
+    pestañas.add(pestaña3, text="Ayuda")
+
+    b1=Button(pestaña1,text="Cargar XML Configuración de Maquina",font=("Verdana",12),borderwidth=5,background="beige").place(x=300,y=250,height=40,width=350)
+    b1=Button(pestaña1,text="Cargar XML Configuración",font=("Verdana",12),borderwidth=5,background="beige").place(x=300,y=300,height=40,width=350)
+
+    pestañas.pack(fill="both",expand="yes")
+
+    
+    
     ventana.mainloop()
 
 
